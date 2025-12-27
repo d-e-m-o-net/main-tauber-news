793b05f682cb28dad96dc06836dfe9d75c74b0b1
d-e-m-o-net <github@d-e-m-o.net>
2025-12-27 12:20:58 +0100

    Update sources.py

--- scraper/sources.py
+++ scraper/sources.py
@@ -1,1 +1,15 @@
+SOURCES = [
+    {
+        "name": "Main-Tauber-Kreis",
+        "url": "https://www.main-tauber-kreis.de/Aktuelles"
+    },
+    {
+        "name": "Stadt Bad Mergentheim",
+        "url": "https://www.bad-mergentheim.de/de/Aktuelles"
+    },
+    {
+        "name": "Stadt Wertheim",
+        "url": "https://www.wertheim.de/startseite/aktuelles"
+    }
+]
 
